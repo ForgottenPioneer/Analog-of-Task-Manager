04b5f4945a61c01691a338f64a1e40a67346a5b5
Student406-11 <Student406-11@NPK-102O-4>
2024-09-26 14:09:47 +0700

    Добавьте файлы проекта.

---
+++ аналог диспетчера задач winforms/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace аналог_диспетчера_задач_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewProcesses = new System.Windows.Forms.DataGridView();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.buttonFilter = new System.Windows.Forms.Button();
+            this.buttonTerminate = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonUserProcesses = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewProcesses
+            // 
+            this.dataGridViewProcesses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProcesses.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewProcesses.Name = "dataGridViewProcesses";
+            this.dataGridViewProcesses.Size = new System.Drawing.Size(646, 150);
+            this.dataGridViewProcesses.TabIndex = 0;
+            this.dataGridViewProcesses.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(122, 182);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilter.TabIndex = 1;
+            // 
+            // buttonFilter
+            // 
+            this.buttonFilter.Location = new System.Drawing.Point(438, 208);
+            this.buttonFilter.Name = "buttonFilter";
+            this.buttonFilter.Size = new System.Drawing.Size(337, 23);
+            this.buttonFilter.TabIndex = 2;
+            this.buttonFilter.Text = "фильтрует процессы по имени при нажатии кнопки \"Фильтр\"";
+            this.buttonFilter.UseVisualStyleBackColor = true;
+            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
+            // 
+            // buttonTerminate
+            // 
+            this.buttonTerminate.Location = new System.Drawing.Point(287, 237);
+            this.buttonTerminate.Name = "buttonTerminate";
+            this.buttonTerminate.Size = new System.Drawing.Size(359, 23);
+            this.buttonTerminate.TabIndex = 3;
+            this.buttonTerminate.Text = " завершает выбранный процесс при нажатии кнопки";
+            this.buttonTerminate.UseVisualStyleBackColor = true;
+            this.buttonTerminate.Click += new System.EventHandler(this.buttonTerminate_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(0, 237);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(281, 23);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.Text = "обновляет список процессов при нажатии кнопки";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonUserProcesses
+            // 
+            this.buttonUserProcesses.Location = new System.Drawing.Point(652, 237);
+            this.buttonUserProcesses.Name = "buttonUserProcesses";
+            this.buttonUserProcesses.Size = new System.Drawing.Size(527, 23);
+            this.buttonUserProcesses.TabIndex = 5;
+            this.buttonUserProcesses.Text = "отображает процессы, запущенные текущим пользователем, при нажатии кнопки \"Процес" +
+    "сы пользователя\"";
+            this.buttonUserProcesses.UseVisualStyleBackColor = true;
+            this.buttonUserProcesses.Click += new System.EventHandler(this.buttonUserProcesses_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1274, 261);
+            this.Controls.Add(this.buttonUserProcesses);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonTerminate);
+            this.Controls.Add(this.buttonFilter);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.dataGridViewProcesses);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridViewProcesses;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Button buttonFilter;
+        private System.Windows.Forms.Button buttonTerminate;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonUserProcesses;
+    }
+}
+
---
+++ аналог диспетчера задач winforms/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace аналог_диспетчера_задач_winforms
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+                LoadProcesses();
+            }
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void LoadProcesses()
+        {
+            var processes = Process.GetProcesses()
+                .Select(p =>
+                {
+                    try
+                    {
+                        return new
+                        {
+                            Name = p.ProcessName,
+                            ID = p.Id,
+                            Memory = p.WorkingSet64 / 1024 / 1024 + " MB",
+                            StartTime = p.StartTime.ToString()
+                        };
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        return new
+                        {
+                            Name = "Access denied",
+                            ID = p.Id,
+                            Memory = "",
+                            StartTime = ""
+                        };
+                    }
+                })
+                .ToList();
+
+            dataGridViewProcesses.DataSource = processes;
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+            {
+                LoadProcesses();
+            }
+
+            private void buttonTerminate_Click(object sender, EventArgs e)
+            {
+                if (dataGridViewProcesses.SelectedRows.Count > 0)
+                {
+                    int processId = (int)dataGridViewProcesses.SelectedRows[0].Cells["ID"].Value;
+                    try
+                    {
+                        Process.GetProcessById(processId).Kill();
+                        MessageBox.Show("Процесс успешно завершен.");
+                        LoadProcesses();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка завершения процесса: " + ex.Message);
+                    }
+                }
+            }
+
+            private void buttonFilter_Click(object sender, EventArgs e)
+            {
+                string filter = textBoxFilter.Text.ToLower();
+                var processes = Process.GetProcesses()
+                    .Where(p => p.ProcessName.ToLower().Contains(filter))
+                    .Select(p => new
+                    {
+                        Name = p.ProcessName,
+                        ID = p.Id,
+                        Memory = p.WorkingSet64 / 1024 / 1024 + " MB",
+                        StartTime = p.StartTime.ToString()
+                    })
+                    .ToList();
+
+                dataGridViewProcesses.DataSource = processes;
+            }
+
+        private void buttonUserProcesses_Click(object sender, EventArgs e)
+        {
+            var currentUserName = Environment.UserName;
+            var processes = Process.GetProcesses()
+                .Where(p =>
+                {
+                    try
+                    {
+                        return p.StartInfo.UserName == currentUserName;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                })
+                .Select(p => new
+                {
+                    Name = p.ProcessName,
+                    ID = p.Id,
+                    Memory = p.WorkingSet64 / 1024 / 1024 + " MB",
+                    StartTime = p.StartTime.ToString()
+                })
+                .ToList();
+
+            dataGridViewProcesses.DataSource = processes;
+        }
+    }
+
+
+    }
+    
+
